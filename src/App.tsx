0ea18e452bced4e8980751ddcba469cf8d6bab3c
physicsSorcererKing <psk@psk.rocks>
2025-01-23 21:27:25 +0800

    fix: wrong dep arr for addCompareId

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,18 @@ export const App: FC = () => {
   const [compareIds, setCompareIds] = useState<number[]>([]);
   const disableAddToCompare = compareIds.length >= 3;
 
-  const addCompareId = useCallback((id: number) => {
-    setCompareIds((prev) => {
-      if (prev.includes(id) || disableAddToCompare) {
-        return prev;
-      }
+  const addCompareId = useCallback(
+    (id: number) => {
+      setCompareIds((prev) => {
+        if (prev.includes(id) || disableAddToCompare) {
+          return prev;
+        }
 
-      return [...prev, id];
-    });
-  }, []);
+        return [...prev, id];
+      });
+    },
+    [disableAddToCompare],
+  );
 
   const removeCompareId = useCallback((id: number) => {
     setCompareIds((prev) => prev.filter((prevId) => prevId !== id));
@@ -36,7 +39,7 @@ export const App: FC = () => {
         addCompareId={addCompareId}
         disableAddToCompare={disableAddToCompare}
       />
-      <hr className={'border-b w-full h-[1px]'} />
+      <hr className={'border-b w-full h-[1px] my-4'} />
       <CompareTable compareIds={compareIds} />
       <CompareBar
         compareIds={compareIds}
