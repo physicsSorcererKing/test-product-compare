8c87e6ea30a807c7b72268ae3d104d965b9b9163
physicsSorcererKing <psk@psk.rocks>
2025-01-23 21:26:20 +0800

    feat: CompareTable layout

--- src/components/CompareTable.tsx
+++ src/components/CompareTable.tsx
@@ -1,22 +1,36 @@
+import { MOCK_PRODUCTS } from '@/utils/const/data.ts';
+
 type CompareTableProps = {
   compareIds: number[];
 };
 export const CompareTable: React.FC<CompareTableProps> = ({ compareIds }) => {
-  const items = compareIds.map((id) => (
-    <tr key={id}>
-      <td>{id}</td>
-      <td>Product {id}</td>
-      <td>${id * 100}</td>
-    </tr>
-  ));
+  const items = compareIds.map((id) => {
+    const targetProduct = MOCK_PRODUCTS.find((elem) => elem.id === id);
+
+    return targetProduct ? (
+      <tr key={id} className={'text-end'}>
+        <td>{targetProduct.name}</td>
+        <td>${targetProduct.price}</td>
+        <td>
+          <ul className={'text-start'}>
+            {Object.entries(targetProduct.specs).map(([key, value]) => (
+              <li key={key}>
+                {key}: {value}
+              </li>
+            ))}
+          </ul>
+        </td>
+      </tr>
+    ) : null;
+  });
 
   return (
-    <table>
+    <table className={'border-4 border-spacing-4 border-collapse'}>
       <thead>
         <tr>
-          <th>Id</th>
-          <th>Name</th>
-          <th>Price</th>
+          <th className={'min-w-10'}>Name</th>
+          <th className={'min-w-10'}>Price</th>
+          <th className={'min-w-10'}>Specs</th>
         </tr>
       </thead>
       {items}
