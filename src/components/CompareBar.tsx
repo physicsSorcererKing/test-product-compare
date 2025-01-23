a5028e0f47d55defeee35ee776d83d7571b34bdc
physicsSorcererKing <psk@psk.rocks>
2025-01-23 21:08:18 +0800

    feat: CompareBar layout

--- src/components/CompareBar.tsx
+++ src/components/CompareBar.tsx
@@ -1,3 +1,6 @@
+import { ProductItem } from '@/components/ProductItem.tsx';
+import { MOCK_PRODUCTS } from '@/utils/const/data.ts';
+
 export type CompareBarProps = {
   compareIds: number[];
   removeCompareId: (id: number) => void;
@@ -8,22 +11,29 @@ export const CompareBar: React.FC<CompareBarProps> = ({
   removeCompareId,
   clearCompareIds,
 }) => {
-  const items = compareIds.map((id) => (
-    <li key={id}>
-      {id}
-      <button onClick={() => removeCompareId(id)}>Remove</button>
-    </li>
-  ));
+  const items = compareIds.map((id) => {
+    const targetProduct = MOCK_PRODUCTS.find((elem) => elem.id === id);
+    return targetProduct ? (
+      <ProductItem product={targetProduct} key={id}>
+        <button
+          className={'bg-orange-600 text-white'}
+          onClick={() => removeCompareId(id)}
+        >
+          Remove
+        </button>
+      </ProductItem>
+    ) : null;
+  });
 
   return (
     <footer
       className={
-        'fixed bottom-0 left-0 w-full h-[200px] bg-gray-300 flex gap-4'
+        'fixed bottom-0 left-0 w-full h-[200px] pl-4 bg-gray-300 flex gap-4 items-center'
       }
     >
       <ul className={'flex gap-4'}>{items}</ul>
       <button
-        className={'bg-red-600 text-white p-4 ml-auto'}
+        className={'bg-red-600 text-white p-4 ml-auto self-stretch'}
         onClick={clearCompareIds}
       >
         Clear
